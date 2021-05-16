cfc265ff090985d476e4c1812b0f300cacf03497
Yunus Emre Karakuzu <yunusemrekarakuzu@Yunus-MacBook-Pro.local>
2021-05-16 22:18:12 +0300

    dynamic router-link

--- src/components/UserDetail.vue
+++ src/components/UserDetail.vue
@@ -3,6 +3,15 @@
     <h3>User Detail Component</h3>
     <hr>
     <p>Kullanıcının ID Bilgisi : {{$route.params.id}}</p>
+    <router-link
+    tag="button"
+    class="btn btn-primary"
+    :to="{name:'userEdit',params:{id:$route.params.id}}"
+    >
+    <!-- :to="'/user/'+$route.params.id+'/edit'" -->
+    <!-- BU ŞEKİLDE DE TANIMLANABİLİR -->
+    Kullanıcıyı Düzenle
+    </router-link>
   </div>
 </template>
 <script>
