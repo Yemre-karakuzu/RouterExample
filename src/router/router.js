eb31edb5442ff2602a5fa85197a251c269969173
Yunus Emre Karakuzu <yunusemrekarakuzu@Yunus-MacBook-Pro.local>
2021-05-16 23:55:09 +0300

    lazyload

--- src/router/router.js
+++ src/router/router.js
@@ -1,10 +1,41 @@
 // import { component } from "vue/types/umd";
 import Home from "../components/Home"
-import User from "../components/User"
-import UserEdit from "../components/UserEdit"
-import UserDetail from "../components/UserDetail"
-import UserStart from "../components/UserStart"
 import Header from "../components/Header"
+
+
+
+
+// import User from "../components/User"
+// import UserEdit from "../components/UserEdit"
+// import UserDetail from "../components/UserDetail"
+// import UserStart from "../components/UserStart"
+// import { resolve } from "core-js/fn/promise"
+
+
+const User= resolve=>{
+    require.ensure(["../components/User.vue"],()=>{
+        resolve(require("../components/User.vue"));
+    })
+}
+const UserStart= resolve=>{
+    require.ensure(["../components/UserStart.vue"],()=>{
+        resolve(require("../components/UserStart.vue"));
+    })
+}
+
+const UserEdit= resolve=>{
+    require.ensure(["../components/UserEdit.vue"],()=>{
+        resolve(require("../components/UserEdit.vue"));
+    })
+}
+
+const UserDetail= resolve=>{
+    require.ensure(["../components/UserDetail.vue"],()=>{
+        resolve(require("../components/UserDetail.vue"));
+    })
+}
+
+
 
 export const routes=[
     {path:'/',name:"Home",components:{
